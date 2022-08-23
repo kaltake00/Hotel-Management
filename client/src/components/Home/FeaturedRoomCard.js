372b7b235558c9ad19dc0c2b54abacd76373e8cc
Elvedin Kaltak <74422974+kaltake00@users.noreply.github.com>
2022-08-23 13:02:17 +0200

    added featured rooms on homepage

---
+++ client/src/components/Home/FeaturedRoomCard.js
@@ -0,0 +1,41 @@
+import React from 'react'
+import styles from './FeaturedRoomCard.module.css'
+import {HiArrowRight} from 'react-icons/hi'
+import {AiOutlineEdit} from 'react-icons/ai'
+import AuthApi from '../../utils/AuthApi'
+
+function FeaturedRoomCard(props) {
+    const authApi = React.useContext(AuthApi)
+    var isAdmin = false;
+    if (authApi.userData.role === "admin"){
+        isAdmin = true;
+    }
+
+    const roomData = props.roomData;
+
+    return (
+        <div className={styles.roomCard}>
+            <div className={styles.roomImageWrapper}>
+                <img src={roomData.featured_img}></img>
+                {isAdmin && 
+                    <div className={styles.editRoomWrapper}>
+                        <AiOutlineEdit />
+                    </div>
+                }
+            </div>
+            <div className={styles.roomBody}>
+                <div className={styles.roomInformations}>
+                    <p className={styles.roomPrice}>${roomData.price}.00</p>
+                    <h3>{roomData.name}</h3>
+                    <p className={styles.totalBeds}>Beds: {roomData.beds}</p>
+                </div>
+                <div className={styles.roomButton}>
+                    <HiArrowRight />
+                    <p>book</p>
+                </div>
+            </div>
+        </div>
+    )
+}
+
+export default FeaturedRoomCard
--- client/src/components/Home/FrontPage.js
+++ client/src/components/Home/FrontPage.js
@@ -2,6 +2,7 @@ import React, { useState, useEffect } from 'react'
 import axios from 'axios';
 import Header from '../UI/Header'
 import styles from './FrontPage.module.css'
+import FeaturedRoomCard from './FeaturedRoomCard';
 
 function FrontPage() {
 
@@ -15,7 +16,7 @@ function FrontPage() {
     const getFeaturedRooms = async () => {
         const result = await axios.get("http://localhost:3001/rooms/featured")
         setFeaturedRooms(result.data)
-        console.table(featuredRooms)
+        console.log('Featured rooms: ',featuredRooms)
     }
 
     return (
@@ -25,7 +26,7 @@ function FrontPage() {
 
             {/* Here goes body of page */}
             <div className={`container ${styles.homeBanner}`}>
-                <div>
+                <div style={{marginLeft: "75px"}}>
                     <h1>Enjoy Your <br /> dream vacation</h1>
                     <p>Many different rooms with great luxury prepared just for your needs.<br/> Enjoy worry-free in high comfort.</p>
                     <button className={`secondaryButton ${styles.ctaButton}`}>View all available rooms</button>
@@ -34,14 +35,18 @@ function FrontPage() {
 
             {/* Featured Rooms */}
 
-            <div className={styles.featuredRoom_section}>
-                <div>
+            <div className={`container ${styles.featuredRoom_section}`}>
+                <div className={styles.sectionBreadcrumb}>
                     <h3>Featured rooms</h3>
                     <p>View All Rooms</p>
                 </div>
-                <div className='featuredRooms--wrapper'>
-                    {/* Here goes featuredRoom */}
-                    <h1>Sorry, there are no featured rooms!</h1>
+                <div className={styles.featuredRoom_wrapper}>
+                    <div className={styles.featuredRooms_inner}>
+                        {featuredRooms.length > 0 
+                            ? featuredRooms.map(room => <FeaturedRoomCard key={room.id} roomData={room}/>) 
+                            : <h1>There are no rooms</h1>
+                        }
+                    </div>
                 </div>
             </div>
         </div>
